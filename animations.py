8cc04b80f85fa730ec70a8e5e813462c26784f5d
yettii <Achi23@thehill.org>
2023-11-22 16:08:15 -0500

    animations file

---
+++ animations.py
@@ -0,0 +1,19 @@
+from cmu_graphics import *
+
+
+def onAppStart(app):
+    pass
+
+def drawBoard(app):
+    pass
+
+def onMouseDrag(app, mouseX, mouseY):
+    pass
+
+def onMousePress(app, mouseX, mouseY):
+    pass
+
+def redrawAll(app):
+    pass
+
+
